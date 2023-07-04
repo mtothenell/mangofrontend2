ad540ce7b9dd6e380f351dfb0819ab110e09d19d
mnell <mtothenell@gmail.com>
2023-07-04 19:28:01 +0200

    ..00

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
+import MemberTable from "./Components/MemberTable";
 
 function App() {
     return (
-
         <>
-
-
-            <div className="jumbotron text-center">
-                <h1>My First Bootstrap Page</h1>
-                <p>Resize this responsive page to see the effect!</p>
+            <div className="jumbotron text-center" style={{ backgroundColor: '#82CD47', minHeight: '30vh', border: 0, margin: 0 }}>
+                <div className="container">
+                    <h1>Mango padel</h1>
+                    <p>En community för dem som älskar padel!</p>
+                </div>
             </div>
 
-            <nav className="navbar navbar-inverse">
+            <nav className="navbar navbar-inverse" style={{ height: '5vh', border: 0, margin: 0 }}>
                 <div className="container-fluid">
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -19,66 +19,35 @@ function App() {
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
-                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a className="navbar-brand" href="#">WebSiteName</a>
+                        <a className="navbar-brand" href="#">Mango</a>
                     </div>
                     <div className="collapse navbar-collapse" id="myNavbar">
                         <ul className="nav navbar-nav">
-                            <li className="active"><a href="#">Home</a></li>
-                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <li><a href="#">Page 2</a></li>
-                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <li><a href="#">Page 3</a></li>
+                            <li className="active"><a href="#">Medlemmar</a></li>
+                            <li><a href="#">Padelnytt</a></li>
+                            <li><a href="#">Om oss</a></li>
                         </ul>
                     </div>
                 </div>
             </nav>
 
-            <div className="container">
+            <div className="container-fluid" style={{ backgroundColor: '#F0FF42', minHeight: '30vh', margin: 0 }}>
                 <div className="row">
-                    <div className="col-sm-4">
-                        <h2>About Me</h2>
-                        <h5>Photo of me:</h5>
-                        <div className="fakeimg">Fake Image</div>
-                        <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-                        <h3>Some Links</h3>
-                        <p>Lorem ipsum dolor sit ame.</p>
-                        <ul className="nav nav-pills nav-stacked">
-                            <li className="active"><a href="#">Link 1</a></li>
-                            <li><a href="#">Link 2</a></li>
-                            <li><a href="#">Link 3</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-sm-8">
-                        <h2>TITLE HEADING</h2>
-                        <h5>Title description, Dec 7, 2017</h5>
-                        <div className="fakeimg">Fake Image</div>
-                        <p>Some text..</p>
-                        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco.</p>
-                            <h2>TITLE HEADING</h2>
-                            <h5>Title description, Sep 2, 2017</h5>
-                            <div className="fakeimg">Fake Image</div>
-                            <p>Some text..</p>
+                    <div className="col-sm-12">
+                        <h2>Medlemmar</h2>
+                        <MemberTable />
                     </div>
                 </div>
             </div>
 
-            <div className="jumbotron text-center" style={{ marginBottom: 0 }}>
-                <h1>My First Bootstrap Page</h1>
-                <p>Resize this responsive page to see the effect!</p>
+            <div className="jumbotron text-center" style={{ marginBottom: 0, backgroundColor: "#379237", minHeight: '30vh', margin: 0 }}>
+                <div className="container">
+                    <h1>Reklamplats</h1>
+                    <p>Gör reklam för ditt företag här!</p>
+                </div>
             </div>
-
         </>
-
-    )
-        ;
+    );
 }
-
-//    <HamburgerMenu/>
-//    <p>Mango padel - En community för alla som älskar padel</p>
-//    <MemberTable/>
-
 
 export default App;
--- src/Components/MemberTable.js
+++ src/Components/MemberTable.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 
 export default function MemberTable() {
     return (
-        <MDBTable>
-            <MDBTableHead>
-                <tr>
-                    <th scope='col'>#</th>
-                    <th scope='col'>Name</th>
-                    <th scope='col'>Username</th>
-                    <th scope='col'>Nickname</th>
-                </tr>
-            </MDBTableHead>
-            <MDBTableBody>
-                <tr>
-                    <th scope='row'>1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope='row'>2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope='row'>3</th>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </MDBTableBody>
-        </MDBTable>
+        <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Förnamn</th>
+                <th scope="col">Efternamn</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+            </tr>
+            </tbody>
+        </table>
     );
 }
