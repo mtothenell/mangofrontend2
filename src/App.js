205a26baabb19b7f0026c9c4375b86ed31c55900
mnell <mtothenell@gmail.com>
2023-07-06 13:04:16 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import MemberTable from "./Components/MemberTable";
 function App() {
     return (
         <>
-            <div className="jumbotron text-center" style={{ backgroundColor: '#82CD47', minHeight: '30vh', border: 0, margin: 0 }}>
+            <div className="jumbotron text-center" style={{ backgroundColor: '#B5C99A', minHeight: '30vh', border: 0, margin: 0, borderRadius: 10 }}>
                 <div className="container">
                     <h1>Mango padel</h1>
-                    <p>En community för dem som älskar padel!</p>
+                    <p>En community för oss som älskar padel!</p>
                 </div>
             </div>
 
-            <nav className="navbar navbar-inverse" style={{ height: '5vh', border: 0, margin: 0 }}>
+            <nav className="navbar navbar-inverse" style={{ height: '5vh', border: 0, margin: 0, borderRadius: 10, backgroundColor: "#485635"}}>
                 <div className="container-fluid">
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -21,17 +21,21 @@ function App() {
                         </button>
                         <a className="navbar-brand" href="#">Mango</a>
                     </div>
-                    <div className="collapse navbar-collapse" id="myNavbar">
+                    <div className="collapse navbar-collapse" id="myNavbar" >
                         <ul className="nav navbar-nav">
-                            <li className="active"><a href="#">Medlemmar</a></li>
+                            <li className="active" ><a href="#">Medlemmar</a></li>
                             <li><a href="#">Padelnytt</a></li>
+                            <li><a href="#">Turneringar</a></li>
+                            <li><a href="#">Padelresa</a></li>
+                            <li><a href="#">Matchi</a></li>
+                            <li><a href="#">Webshop</a></li>
                             <li><a href="#">Om oss</a></li>
                         </ul>
                     </div>
                 </div>
             </nav>
 
-            <div className="container-fluid" style={{ backgroundColor: '#F0FF42', minHeight: '30vh', margin: 0 }}>
+            <div className="container-fluid" style={{ backgroundColor: "#FFF9C9", minHeight: '34vh', margin: 0, borderRadius: 10 }}>
                 <div className="row">
                     <div className="col-sm-12">
                         <h2>Medlemmar</h2>
@@ -40,10 +44,10 @@ function App() {
                 </div>
             </div>
 
-            <div className="jumbotron text-center" style={{ marginBottom: 0, backgroundColor: "#379237", minHeight: '30vh', margin: 0 }}>
+            <div className="jumbotron text-center" style={{ marginBottom: 0, backgroundColor: "#862B0D", minHeight: '30vh', margin: 0, borderRadius: 10 }}>
                 <div className="container">
-                    <h1>Reklamplats</h1>
-                    <p>Gör reklam för ditt företag här!</p>
+                    <h1>Under konstruktion...</h1>
+                    <p>Just nu inte anpassad för mobilvy!</p>
                 </div>
             </div>
         </>
