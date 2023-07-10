91c910fd9522e04ba70d710fe4b732bef33e9e10
mnell <mtothenell@gmail.com>
2023-07-10 18:06:31 +0200

    ......

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ function App() {
 
     return (
         <>
-            <div className="page">
-                <main className="content">
-                    <article>
-                        <p>
-                            This is an article of content that sits inline with a sidebar. Resize the
-                            browser to see how when there’s no enough room, the sidebar will stack on to a
-                            new line.
-                        </p>
-                    </article>
-                </main>
-                <aside className="sidebar">
-                    <p>This sidebar will stack where there is not enough room.</p>
-                </aside>
+            <div className="container">
+                <div className="item" id="item-1"></div>
+                <div className="item" id="item-2"></div>
+                <div className="item" id="item-3"></div>
             </div>
         </>
     );
